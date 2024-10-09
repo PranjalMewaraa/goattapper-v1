d87bae7fb244ca817c15c6fe5cb6d820fdc621ce
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-10-09 13:02:17 +0530

    n

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -5,7 +5,7 @@ const Earn: React.FC = () => {
   return (
     <div className='w-full p-4 flex items-center h-full'>
       <div id='earn' className='w-full h-4/5 flex flex-col text-white gap-2'>
-        <h1 className='w-full text-center font-[ageobold] py-2 text-4xl'>Earn More Coins!</h1>
+        <h1 className='w-full text-center font-[ageobold] pt-8 text-4xl'>Earn More Coins!</h1>
         <p className='w-full text-center'>Complete exciting tasks and earn coins</p>
         <div className='w-full flex flex-col gap-2 p-4'>
           <CardElem name="Invite a Friend" />
